43fe17c0c21e99b1e8d17d3fdf20847c2e5e6b34
bbouya <52068209+bbouya@users.noreply.github.com>
2022-04-01 11:49:42 +0000

    files

---
+++ 01 reverse.js
@@ -0,0 +1,8 @@
+function reverseString(str){
+    return str.split('').reverse().join('');
+
+}
+
+
+reverse = reverseString('ayoub el ghafraoui');
+print(reverse);
---
+++ 02 factorialize.js
@@ -0,0 +1,12 @@
+function factorialize(num){
+
+    var prod = 1;
+    for(var i =1; i<= num ; i++){
+        prod*=i;
+    }
+    return prod;
+}
+
+fact = factorialize(5);
+console.log(fact);
+print(fact)
